42c4e40858ee06de99c78ee9dc46fd712314cfc8
scasasr <CORREO@example.com>
2023-03-06 02:51:37 -0500

    adding bill responsive format, cluster field Ap

--- src/layouts/Bill.js
+++ src/layouts/Bill.js
@@ -1,9 +1,7 @@
 import React, {useState,useEffect} from "react";
 import style from '../components/totalCart/ItemCart/style.module.scss'
 import Navbar_all from "../components/Navbar.js";
-import GoogleMap  from "simple-react-google-maps";
 import { Divider } from "@mui/material";
-import qs from "qs";
 
 //Icons 
 import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
@@ -11,39 +9,25 @@ import NotesIcon from '@mui/icons-material/Notes';
 
 
 import API from "../services/http-common.js";
+import Cookies from "universal-cookie";
 
 
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 
 const Bill = () =>{
+    //cookie
+    var cookie = new Cookies();
 
     const [delivery,setDelivery] = useState(0);
     const [service, setService] = useState(0);
-
+    const [userId, setUserId] = useState(cookie.get('user_id'));
     const [sendInformation,setSendInformation]=useState(false);
-    //data Tu compra 
-    const [terminal,setTerminal] = useState('jum96fe3r2j4xxsq');
-    const [bill,setBill] = useState(1236554800);
-    const [description,setDescription] = useState('Productos prueba');
-
-    //data Tu compra user 
-    const [documentNumber,setDocumentNumber] = useState('1089236522');
-    const [documentType,setDocumentType] = useState('CC');
-    const [userName,setUSerName] = useState('Perdro');
-    const [userLastName,setUSerLastName] = useState('Gonzales');
-    const [userEmail,setUserEmail] = useState('pgonzales@gmail.com');
-
- 
-
     
-
-    //geolocation
-    const [latitude, setLatitude] = useState('')
-    const [longitude, setLongitude] = useState('')
-    //
- 
+    //data order 
+    const [address,setAddress] = useState('');
+    const [details,setDetails] = useState('');
+  
     const [cartItems, setCartItems] = useState(()=>{
         try {
             const localStoragePosts = localStorage.getItem("cartProducts");
@@ -51,6 +35,14 @@ const Bill = () =>{
         } catch (error) {
             return [];
         }
+    });
+
+    const [description,setDescription] = useState(() =>{
+        var descriptions = 'Pago ';
+        cartItems.map((item,i) => (
+            descriptions = (descriptions + item.description + ' - ')
+        ))
+        return descriptions
     });
 
 
@@ -62,117 +54,67 @@ const Bill = () =>{
 
     const [total,setTotal] = useState(totalProducts + delivery + service);
 
-    // const test = () =>{
-    //     // const data =JSON.stringify(values);
-    //     // const data =utf8.encode(values);
-    //     console.log('sending');
-    //     const params = new URLSearchParams();
-    //     params.append('usuario', 'jum96fe3r2j4xxsq');
-    //     params.append('factura', '1230569');
-    //     params.append('valor', '100000');
-    //     params.append('descripcionFactura', 'PruebaEnvio');
-    //     console.log(params);
-    //    const options = {
-    //     method:'POST',
-    //     headers:{'content-type': 'application/x-www-form-urlencoded'},
-    //     data:params,
-    //     url:"https://demover3-1.tucompra.net/tc3/app/inputs/compra.jsp"
-    //    }
-    //    setSendInformation(false);
-    //    return axios(options).then((response) => {
-    //     console.log(response.status)
-    //    }).catch((error)=>{
-    //     console.log(error)
-    //    });
-    // }
+     
 
-    // const test = (values) => {
-    //     const data = qs.stringify(values)
-    //     console.log(data);
-    //     axios.post("https://demover3-1.tucompra.net/tc3/app/inputs/compra.jsp",data)
-
-    // }
-
-    const test = () =>{
-        var data = qs.stringify({
-            'usuario': 'jum96fe3r2j4xxsq',
-            'factura': '10236589',
-            'valor': '100000',
-            'descripcionFactura': 'postmanTest' 
-          });
-        console.log(data)
-        var config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://demover3-1.tucompra.net/tc3/app/inputs/compra.jsp',
-        headers: { 
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data : data
-        };
-        
-        axios(config)
-        .then(function (response) {
-        console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
+    const goTuCompra = (bill) =>{
+      const values= {
+        bill:bill,
+        total:total,
+        description:description,
+        userId:userId,
+        address:address
+      }
+      console.log(values);
+      const data = JSON.stringify(values);
+      API.post('conections/TuCompra',data).then((response) => {
+        if(response.status === 200){
+            console.log(response.data);
+            window.location.href = response.data;
+        }
+      }).catch((error) => {
         console.log(error);
-        });
+      })
     }
-    
 
-    useEffect(() => {
-        test();
-    }, [sendInformation]);
-
-    //geolocation
-    const geolocationVer = () =>{
-        if ('geolocation' in navigator) {
-            getCoordinates();
-        } else {
-            console.log({geo:false})
+    const sendOrder = () =>{  
+        const values = {
+            buyer:userId,
+            address:address,
+            details:details,
+            transaction:'63ff7f984034a758d059579b',//default value, backend method modificate this value
+            products:cartItems,
+            total:total
+        }
+        const data = JSON.stringify(values);
+        API.post('orders/add',data).then((response) => {
+          if(response.status === 201){
+              goTuCompra(response.data);
+              document.getElementById("Address").value = "";
+              document.getElementById("Details").value = "";
+          }
+        }).catch((error) => {
+          console.log(error);
+        })
+    } 
+      
+    const onChangeAddress= (e) => {
+        if(e.target.value && e.target.value.length > 0){
+            setAddress(e.target.value); 
         }
     }
 
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-
-    function error_geo(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
+    const onChangeDetails= (e) => {
+        if(e.target.value && e.target.value.length > 0){
+            setDetails(e.target.value); 
+        }else{
+            setDetails('nodetails');
+        }
     }
-
-    const getCoordinates = () =>{
-        navigator.geolocation.getCurrentPosition((position) => {
-            // console.log("Latitude is :", position.coords.latitude);
-            setLatitude(position.coords.latitude);
-            // console.log("Longitude is :", position.coords.longitude);
-            setLongitude(position.coords.longitude);
-        },error_geo,options);
-
-    }
-
-    useEffect(() => {
-        geolocationVer();  
-    }, []);
-
-    // const dataTest = {
-    //     usuario:terminal,
-    //     factura:bill,
-    //     valor:total,
-    //     descripcionFactura:description,
-    //     documentoComprador:documentNumber,
-    //     tipoDocumento:documentType,
-    //     nombreComprador:userName,
-    //     apellidoComprador:userLastName,
-    //     correoComprador:userEmail
-    // }
 
     const itemBill = (item) =>{
         const photo = item.photo
         return(<>
+            
             <div className={style.cartItem}>
                 <img src={photo} alt={item.description}/>
                 <div className={style.dataContainer}>
@@ -201,30 +143,14 @@ const Bill = () =>{
         {Navbar_all("lock_person","how_to_reg","/login","RoleSelect","Entrar","Registrarse")}
         {/*NAVBAR END*/}
         <div>
-            <div className="d-flex justify-content-around mt-5">
+            <div className="billContainer mt-5">
                 <div className="pb-5">
                     <div className="summary_container_1 pb-4">
                         <div className="summary_container">
                             <h5 className="py-2" style={{color:"#fff"}}>Dirección de entrega</h5>
                         </div>
                         <div>
-                            <div className="d-flex mt-4">
-                                <div className="m-4">
-                                    <GoogleMap
-                                            apiKey={"AIzaSyBFkZKPPLySYchQ4VJHFXczriU9BwmHawQ"}
-                                            style={{height:"300px",width:"370px"}}
-                                            zoom={15}
-                                            center={{
-                                                lat:Number(latitude),
-                                                lng:Number(longitude),
-                                            }}
-                                            markers={[
-                                                {lat:Number(latitude),
-                                                lng:Number(longitude),}
-                                            ]}
-                                        />
-                                </div>
-
+                            <div className="inputContainer mt-4">
                                 <div className="input-group my-3 mr-3" style={{height:"38px"}}>
                                     <span className="input-group-text"><LocationSearchingIcon/></span>
                                     <input 
@@ -232,12 +158,14 @@ const Bill = () =>{
                                         type="text" 
                                         id="Address" 
                                         name= "Address" 
-                                        placeholder="Direccion"
+                                        placeholder="Dirección"
+                                        onChange={onChangeAddress}
                                     />
                                 </div>       
                             </div>
                             <Divider/>
                             <p className="ml-3 mt-5 d-flex justify-content-start">Indicaciones de la entrega (opcional)</p>
+                            
                             <div className="input-group my-3">
                                 <span className="input-group-text"><NotesIcon/></span>
                                 <input 
@@ -246,6 +174,7 @@ const Bill = () =>{
                                     id="Details" 
                                     name= "Details" 
                                     placeholder="Indicaciones entrega (Ejm: Bloque,Apto,Casa)"
+                                    onChange={onChangeDetails}
                                 />
                             </div>
                         </div>
@@ -292,7 +221,7 @@ const Bill = () =>{
                     </div>
                     <Divider/>
                     
-                    <button className="btn btn-success mt-4" onClick={() => setSendInformation(true)}>Pagar</button>
+                    <button className="btn btn-success mt-4" onClick={() => sendOrder()} >Pagar</button>
                     
                         
                     
